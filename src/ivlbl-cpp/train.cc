b78e7685835e733f6f0285770422181d77ceaa44
Manaal Faruqui <manaalfar@gmail.com>
2013-12-24 20:25:36 -0500

    replaced all floats by doubles and ints by unsigned

--- src/ivlbl-cpp/train.cc
+++ src/ivlbl-cpp/train.cc
@@ -26,19 +26,19 @@
 using namespace std;
 using namespace Eigen;
 
-typedef std::tr1::unordered_map<string, int> mapStrInt;
+typedef std::tr1::unordered_map<string, unsigned> mapStrunsigned;
 typedef std::tr1::unordered_map<string, string> mapStrStr;
-typedef std::tr1::unordered_map<int, float> mapIntFloat;
-typedef std::tr1::unordered_map<int, int> mapIntInt;
+typedef std::tr1::unordered_map<unsigned, double> mapunsigneddouble;
+typedef std::tr1::unordered_map<unsigned, unsigned> mapunsignedunsigned;
 
 /* =================== Utility functions begin =================== */
 
-float EPSILON = 0.00000000000000000001;
+double EPSILON = 0.00000000000000000001;
 
 string normalize_word(string& word) {
     if (std::string::npos != word.find_first_of("0123456789"))
         return "---num---";
-    for (int i=0; i<word.length(); ++i)
+    for (unsigned i=0; i<word.length(); ++i)
         if (isalnum(word[i])){
             transform(word.begin(), word.end(), word.begin(), ::tolower);
             return word;
@@ -58,16 +58,16 @@ vector<string> split_line(string& line, char delim) {
     return words;
 }
 
-pair<mapStrInt, mapStrStr> get_vocab(string filename) {
+pair<mapStrunsigned, mapStrStr> get_vocab(string filename) {
     string line, normWord;
     vector<string> words;
-    mapStrInt vocab;
+    mapStrunsigned vocab;
     mapStrStr word2norm;
     ifstream myfile(filename.c_str());
     if (myfile.is_open()) {
         while(getline(myfile, line)) {
             words = split_line(line, ' ');
-            for(int i=0; i<words.size(); i++){
+            for(unsigned i=0; i<words.size(); i++){
                 normWord = normalize_word(words[i]);
                 if (word2norm.find(words[i]) == word2norm.end())
                     word2norm[words[i]] = normWord;
@@ -83,97 +83,97 @@ pair<mapStrInt, mapStrStr> get_vocab(string filename) {
 
 /* 
 It is not deleting stuff, dont know why !
-while printing its still there ! x-(
+while prunsigneding its still there ! x-(
 http://stackoverflow.com/questions/17036428/c-map-element-doesnt-get-erased-if-i-refer-to-it
 */
-mapStrInt filter_vocab(mapStrInt& vocab, const int freqCutoff) {
-    mapStrInt filtVocab;
-    for (mapStrInt::iterator it = vocab.begin(); it != vocab.end(); ++it)
+mapStrunsigned filter_vocab(mapStrunsigned& vocab, const unsigned freqCutoff) {
+    mapStrunsigned filtVocab;
+    for (mapStrunsigned::iterator it = vocab.begin(); it != vocab.end(); ++it)
         if (! (it->second < freqCutoff) )
             filtVocab[it->first] = it->second;
     return filtVocab;
 }
 
-mapStrInt reindex_vocab(mapStrInt& vocab) {
-    int index = 0;
-    mapStrInt indexedVocab;
-    for (mapStrInt::iterator it = vocab.begin(); it != vocab.end(); ++it)
+mapStrunsigned reindex_vocab(mapStrunsigned& vocab) {
+    unsigned index = 0;
+    mapStrunsigned indexedVocab;
+    for (mapStrunsigned::iterator it = vocab.begin(); it != vocab.end(); ++it)
         indexedVocab[it->first] = index++;
     return indexedVocab;
 }
 
-mapIntFloat get_log_unigram_dist(mapStrInt& vocab, mapStrInt& indexedVocab) {
-    float sumFreq = 0;
-    mapIntFloat unigramDist;
-    for (mapStrInt::iterator it = vocab.begin(); it != vocab.end(); ++it)
+mapunsigneddouble get_log_unigram_dist(mapStrunsigned& vocab, mapStrunsigned& indexedVocab) {
+    double sumFreq = 0;
+    mapunsigneddouble unigramDist;
+    for (mapStrunsigned::iterator it = vocab.begin(); it != vocab.end(); ++it)
         sumFreq += it->second;
-    for (mapStrInt::iterator it = vocab.begin(); it != vocab.end(); ++it)
+    for (mapStrunsigned::iterator it = vocab.begin(); it != vocab.end(); ++it)
         unigramDist[indexedVocab[it->first]] = log(it->second/sumFreq);
     return unigramDist;
 }
 
-void print_map(mapStrInt& vocab) {
-    for (mapStrInt::iterator it = vocab.begin(); it != vocab.end(); ++it)
+void prunsigned_map(mapStrunsigned& vocab) {
+    for (mapStrunsigned::iterator it = vocab.begin(); it != vocab.end(); ++it)
         cout << it->first << " " << it->second << "\n";
 }
 
-void print_map(mapIntFloat& vocab) {
-    for (mapIntFloat::iterator it = vocab.begin(); it != vocab.end(); ++it) 
+void prunsigned_map(mapunsigneddouble& vocab) {
+    for (mapunsigneddouble::iterator it = vocab.begin(); it != vocab.end(); ++it) 
         cout << it->first << " " << it->second << "\n";
 }
 
-void print_map(mapStrStr& vocab) {
+void prunsigned_map(mapStrStr& vocab) {
     for (mapStrStr::iterator it = vocab.begin(); it != vocab.end(); ++it) 
         cout << it->first << " " << it->second << "\n";
 }
 
-RowVectorXf epsilon_vector(int row) {
+RowVectorXf epsilon_vector(unsigned row) {
     RowVectorXf nonZeroVec(row);
     nonZeroVec.setOnes(row);
     nonZeroVec *= EPSILON;
     return nonZeroVec;
 }
 
-vector<RowVectorXf> epsilon_vector(int row, int col) {
+vector<RowVectorXf> epsilon_vector(unsigned row, unsigned col) {
     vector<RowVectorXf> epsilonVec;
     RowVectorXf vec = epsilon_vector(col);
-    for (int i=0; i<row; ++i)
+    for (unsigned i=0; i<row; ++i)
         epsilonVec.push_back(vec);
     return epsilonVec;
 }
 
-RowVectorXf random_vector(const int length) {
+RowVectorXf random_vector(const unsigned length) {
     RowVectorXf randVec(length);
-    for (int i=0; i<randVec.size(); ++i)
+    for (unsigned i=0; i<randVec.size(); ++i)
         randVec[i] = (rand()/(double)RAND_MAX);
     randVec /= randVec.norm();
     return randVec;
 }
 
-vector<RowVectorXf> random_vector(int row, int col) {
+vector<RowVectorXf> random_vector(unsigned row, unsigned col) {
     vector<RowVectorXf> randVec;
-    for (int i=0; i<row; ++i){
+    for (unsigned i=0; i<row; ++i){
         randVec.push_back(random_vector(col));
     }
     return randVec;
 }
 
-void print_vectors(char* fileName, vector<RowVectorXf>& wordVectors, 
-                   mapStrInt& indexedVocab) {
+void prunsigned_vectors(char* fileName, vector<RowVectorXf>& wordVectors, 
+                   mapStrunsigned& indexedVocab) {
     ofstream outFile(fileName);
-    for (mapStrInt::iterator it=indexedVocab.begin(); it!= indexedVocab.end(); it++){
+    for (mapStrunsigned::iterator it=indexedVocab.begin(); it!= indexedVocab.end(); it++){
         /* This check is wrong but I have to put it, coz of the elements not getting deleted :(
          By this we will be missing the word at index 0. */
          if (it->second != 0){
             outFile << it->first << " ";
-            for (int i=0; i != wordVectors[it->second].size(); ++i)
+            for (unsigned i=0; i != wordVectors[it->second].size(); ++i)
                 outFile << wordVectors[it->second][i] << " ";
             outFile << "\n";
         }
     }
 }
 
-float logistic(float val) {
+double logistic(double val) {
     if (val > 20) return 1;
     else if (val < -20) return 0.;
     else return 1/(1+exp(-1*val));
@@ -182,9 +182,9 @@ float logistic(float val) {
 /* =================== Utility functions end =================== */
 
 /* Selects words randomly for now */
-vector<int> get_noise_words(int numNoiseWords, int vocabSize) {
-    vector<int> noiseWords;
-    int selectedWords = 0;
+vector<unsigned> get_noise_words(unsigned numNoiseWords, unsigned vocabSize) {
+    vector<unsigned> noiseWords;
+    unsigned selectedWords = 0;
     while (selectedWords != numNoiseWords){
         noiseWords.push_back(rand() % vocabSize);
         ++selectedWords;
@@ -192,27 +192,27 @@ vector<int> get_noise_words(int numNoiseWords, int vocabSize) {
     return noiseWords;
 }
 
-vector<int> words_in_window(vector<int>& words, int wordIndex, 
-                            int windowSize) {
-    vector<int> wordsInWindow;
-    int start, end, sentLen = words.size();
+vector<unsigned> words_in_window(vector<unsigned>& words, unsigned wordIndex, 
+                            unsigned windowSize) {
+    vector<unsigned> wordsInWindow;
+    unsigned start, end, sentLen = words.size();
     start = (wordIndex <= windowSize) ? 0 : wordIndex-windowSize;
     end = (sentLen - wordIndex <= windowSize) ? sentLen-1 : wordIndex + windowSize;
-    for (int i=start; i<wordIndex; ++i)
+    for (unsigned i=start; i<wordIndex; ++i)
         wordsInWindow.push_back(words[i]);
-    for (int i=wordIndex+1; i<=end; ++i)
+    for (unsigned i=wordIndex+1; i<=end; ++i)
         wordsInWindow.push_back(words[i]);
     return wordsInWindow;
 }
 
-float diff_score_word_and_noise(int word, vector<int>& contextWords,
-                                int numNoiseWords, mapIntFloat& noiseDist,
+double diff_score_word_and_noise(unsigned word, vector<unsigned>& contextWords,
+                                unsigned numNoiseWords, mapunsigneddouble& noiseDist,
                                 RowVectorXf& wordBiases,
                                 vector<RowVectorXf >& wordVectors,
-                                float logNumNoiseWords) {                     
-    float sumScore = 0;
-    #pragma omp parallel for reduction(+:sumScore) num_threads(2)
-    for (int i=0; i<contextWords.size(); ++i)
+                                double logNumNoiseWords) {                     
+    double sumScore = 0;
+    #pragma omp parallel for reduction(+:sumScore) num_threads(5)
+    for (unsigned i=0; i<contextWords.size(); ++i)
         sumScore += wordVectors[word].dot(wordVectors[contextWords[i]]) + wordBiases[contextWords[i]];
 
     return sumScore - logNumNoiseWords - noiseDist[word];
@@ -221,11 +221,11 @@ float diff_score_word_and_noise(int word, vector<int>& contextWords,
 /* Main class definition that learns the word vectors */
 
 class WordVectorLearner {
-    mapStrInt vocab;
-    mapIntFloat noiseDist;
+    mapStrunsigned vocab;
+    mapunsigneddouble noiseDist;
     mapStrStr word2norm;
-    int vocabSize, windowSize, numNoiseWords, freqCutoff, vecLen;
-    float learningRate, logNumNoiseWords;
+    unsigned vocabSize, windowSize, numNoiseWords, freqCutoff, vecLen;
+    double learningRate, logNumNoiseWords;
     string corpusName;
     vector<RowVectorXf> adagradVecMem;
     RowVectorXf wordBiases, adagradBiasMem;
@@ -233,8 +233,8 @@ class WordVectorLearner {
   public:
       
     vector<RowVectorXf> wordVectors;
-    mapStrInt indexedVocab;
-    WordVectorLearner(int window, int freq, int noiseWords, int vectorLen) {
+    mapStrunsigned indexedVocab;
+    WordVectorLearner(unsigned window, unsigned freq, unsigned noiseWords, unsigned vectorLen) {
         windowSize = window;
         freqCutoff = freq;
         numNoiseWords = noiseWords;
@@ -243,7 +243,7 @@ class WordVectorLearner {
     }
       
     void preprocess_vocab(string corpusName) {
-        pair<mapStrInt, mapStrStr> vocabPair = get_vocab(corpusName);
+        pair<mapStrunsigned, mapStrStr> vocabPair = get_vocab(corpusName);
         vocab = vocabPair.first;
         word2norm = vocabPair.second;
         cerr << "Orig vocab " << vocab.size() << "\n";
@@ -253,7 +253,7 @@ class WordVectorLearner {
         vocabSize = indexedVocab.size();
     }
     
-    void init_vectors(int vocabSize, int vecLen) {
+    void init_vectors(unsigned vocabSize, unsigned vecLen) {
         wordVectors = random_vector(vocabSize, vecLen); 
         wordBiases = random_vector(vocabSize);
         adagradVecMem = epsilon_vector(vocabSize, vecLen);
@@ -262,34 +262,35 @@ class WordVectorLearner {
     
     void set_noise_dist() { 
         noiseDist = get_log_unigram_dist(vocab, indexedVocab);
+        
     }
     
-    void train_word_vectors(vector<int>& words, float rate) {
-        for (int i=0; i<words.size(); ++i) {
-            vector<int> noiseWords = get_noise_words(numNoiseWords, vocabSize);
+    void train_word_vectors(vector<unsigned>& words, double rate) {
+        for (unsigned i=0; i<words.size(); ++i) {
+            vector<unsigned> noiseWords = get_noise_words(numNoiseWords, vocabSize);
             /* Get the words in the window context of the target word */
-            vector<int> contextWords = words_in_window(words, i, windowSize);
+            vector<unsigned> contextWords = words_in_window(words, i, windowSize);
             /* Get the diff of score of the word in context and the noise dist */
-            float wordContextScore = logistic(diff_score_word_and_noise(words[i], contextWords, numNoiseWords,
+            double wordContextScore = logistic(diff_score_word_and_noise(words[i], contextWords, numNoiseWords,
                                                                   noiseDist, wordBiases, wordVectors, logNumNoiseWords));
             /* Get the diff of score of the noise words in context and the noise dist */     
             RowVectorXf noiseScoreGradProd(wordVectors[0].size());
             noiseScoreGradProd.setZero(noiseScoreGradProd.size());
-            float noiseScoreSum = 0;
-            for (int j=0; j<noiseWords.size(); ++j) {
-                float noiseScore = logistic(diff_score_word_and_noise(noiseWords[j], contextWords, numNoiseWords,
+            double noiseScoreSum = 0;
+            for (unsigned j=0; j<noiseWords.size(); ++j) {
+                double noiseScore = logistic(diff_score_word_and_noise(noiseWords[j], contextWords, numNoiseWords,
                                                                 noiseDist, wordBiases, wordVectors, logNumNoiseWords));
                 noiseScoreSum += noiseScore;
                 noiseScoreGradProd += noiseScore * wordVectors[noiseWords[j]];
             }
             /* Grad wrt bias is one, grad wrt contextWord is the target word */
-            float updateInBias = 1 - wordContextScore - noiseScoreSum;
+            double updateInBias = 1 - wordContextScore - noiseScoreSum;
             RowVectorXf updateInVec = (1 - wordContextScore) * wordVectors[words[i]] - noiseScoreGradProd;
             /* Update adagrad params and add the updates to the context words now */
             RowVectorXf updateVecSquare = updateInVec.array().square();
-            float updateBiasSquare = pow(updateInBias, 2);
-            for (int k=0; k<contextWords.size(); ++k) {
-                int contextWord = contextWords[k];
+            double updateBiasSquare = updateInBias*updateInBias;
+            for (unsigned k=0; k<contextWords.size(); ++k) {
+                unsigned contextWord = contextWords[k];
                 /* Update the adagrad memory first */
                 adagradVecMem[contextWord] += updateVecSquare;
                 adagradBiasMem[contextWord] += updateBiasSquare;
@@ -301,24 +302,24 @@ class WordVectorLearner {
         }
     }
 
-    void train_on_corpus(string corpusName, int iter, float learningRate) {
+    void train_on_corpus(string corpusName, unsigned iter, double learningRate) {
         preprocess_vocab(corpusName);
         init_vectors(vocabSize, vecLen);
         set_noise_dist();
-        for (int i=0; i<iter; ++i) {
-            float rate = learningRate/(i+1);
+        for (unsigned i=0; i<iter; ++i) {
+            double rate = learningRate/(i+1);
             cerr << "Iteration: " << i+1 << "\n";
             cerr << "Learning rate: " << rate << "\n";
             ifstream inputFile(corpusName.c_str());
             string line, normWord, token;
             vector<string> tokens;
-            vector<int> words;
-            int numWords = 0;
+            vector<unsigned> words;
+            unsigned numWords = 0;
             if (inputFile.is_open()) {
                 while (getline(inputFile, line)) {
                     /* Extract normalized words from sentences */
                     tokens = split_line(line, ' ');
-                    for (int i=0; i<tokens.size(); ++i){
+                    for (unsigned i=0; i<tokens.size(); ++i){
                         token = tokens[i];
                         if (word2norm.find(token) != word2norm.end())
                            words.push_back(indexedVocab[word2norm[token]]);
@@ -339,12 +340,12 @@ class WordVectorLearner {
 };
 
 int main(){
-    string corpus = "../news.2011.en.norm";
-    int window = 5, freqCutoff = 10, noiseWords = 10, vectorLen = 80, numIter = 1;
-    float rate = 0.05;
+    string corpus = "../10k";
+    unsigned window = 5, freqCutoff = 2, noiseWords = 10, vectorLen = 80, numIter = 1;
+    double rate = 0.05;
     
     WordVectorLearner obj (window, freqCutoff, noiseWords, vectorLen);
     obj.train_on_corpus(corpus, numIter, rate);
-    print_vectors("news-n2-80.txt", obj.wordVectors, obj.indexedVocab);
+    prunsigned_vectors("news-n5-80.txt", obj.wordVectors, obj.indexedVocab);
     return 1;
 }
